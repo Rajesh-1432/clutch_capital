8a1a2e79d947a73738f3faafe748f58fbd577687
Rajesh-1432 <rajeshmca1432@gmail.com>
2025-05-05 15:47:28 +0530

    minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,7 +43,9 @@ const Home = () => {
                 <div className="w-full md:w-1/2 space-y-6 text-center">
                     <h1 className="text-3xl md:text-3xl  text-blue-950 py-4">Accelerate Your Business Growth!</h1>
                     <p className="text-lg md:text-xl text-blue-950">
-                        Tap into a network of 200+ investors and get capital infusion rapidly and efficiently
+                        Tap into a targeted network of 2000+ high impact investors and get capital infusion rapidly and efficiently.
+                    </p>
+                    <p className="text-lg md:text-xl text-blue-950">Proven track record of fund raises from Seed-stage, Pre-Series A or Series A and beyond and connect with our experts for access to free case studies.
                     </p>
                     <button onClick={() => window.location.href = '/contact'} className="bg-blue-800 text-white py-3 px-8 rounded uppercase font-bold tracking-wider hover:bg-blue-800 transition duration-200 ring-2 ring-blue-800 ring-offset-2 focus:outline-none focus:ring-4">
                         Consult Now
